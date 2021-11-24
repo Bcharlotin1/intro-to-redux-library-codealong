8c8cf7d182ff38456b2560fd695de827291db7ca
bcharlotin1 <biancacharlotin1@gmail.com>
2021-11-24 16:09:01 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import counterReducer from './reducers/counterReducer.js';
+import {createStore} from "redux";
 import App from './App';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const store = createStore(
+    counterReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    //so i can uses the google crhome tool
+    )
+
+
+ReactDOM.render(<App store={store} />, document.getElementById('root'));
